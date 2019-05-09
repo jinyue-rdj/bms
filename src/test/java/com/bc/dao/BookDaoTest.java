48ce0dbca3b23dc9e7fd2b6a5daee43f41290e54
Rong Duojun <rdj2432903@sina.com>
2019-05-09 19:51:50 +0800

    test

--- src/test/java/com/bc/dao/BookDaoTest.java
+++ src/test/java/com/bc/dao/BookDaoTest.java
@@ -11,14 +11,14 @@ public class BookDaoTest extends BaseTest {
     private BookDao bookDao;
 
     @Test
-    public void testQueryById() {
+    public void testQueryById() throws Exception{
         long bookId = 1000;
         Book book = bookDao.queryById(bookId);
         System.out.println(book);
     }
 
     @Test
-    public void testQueryAll() {
+    public void testQueryAll() throws Exception{
         List<Book> books = bookDao.queryAll(0, 4);
         for(Book book : books){
             System.out.println(book);
@@ -26,7 +26,7 @@ public class BookDaoTest extends BaseTest {
     }
 
     @Test
-    public void testReduceNumber() {
+    public void testReduceNumber() throws Exception{
         long bookId = 1000;
         int update = bookDao.reduceNumber(bookId);
         System.out.println("update="+update);
